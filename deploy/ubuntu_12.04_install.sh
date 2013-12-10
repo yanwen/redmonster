ae19452daeb077f2bb953046604fba7cd8f3b017
yanwen <y4nwen@gmail.com>
2013-12-10 10:03:39 +0800

    Update ubuntu_12.04_install.sh
    2.0.247

--- deploy/ubuntu_12.04_install.sh
+++ deploy/ubuntu_12.04_install.sh
@@ -11,23 +11,34 @@ fi
 MYSQL_PASSWD="$1"
 MAIL_ADDRESS="$2"
 
+RUBY_VERSION="2.0.0-p247"
+
 sudo apt-get update
 sudo apt-get install -y unzip curl aptitude vim debconf-utils
+
+# Install RVM
 curl -L get.rvm.io | bash -s stable
 source_rvm="source ~/.rvm/scripts/rvm"
 echo "$source_rvm" >> ~/.bashrc
 echo "$source_rvm" | bash
+
+# Install deps
 sudo apt-get install -y build-essential openssl \
 libreadline6 libreadline6-dev \
 curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev \
 libsqlite3-0  libxml2-dev libxslt-dev \
 autoconf libc6-dev ncurses-dev automake libtool bison g++ \
 libmysqlclient-dev
+
+# Install RMagic deps
 sudo apt-get install -y graphicsmagick-libmagick-dev-compat
 sudo apt-get install -y libmagickwand-dev
-~/.rvm/bin/rvm install ruby-1.9.3-p327
+
+# Install Ruby
+~/.rvm/bin/rvm install $RUBY_VERSION
 echo "$source_rvm" | bash
-rvm use 1.9.3-p327 --default
+rvm use $RUBY_VERSION --default
+
 sudo aptitude install -y memcached imagemagick nodejs nginx
 
 cat <<MYSQL_PRESEED | sudo su -c debconf-set-selections
